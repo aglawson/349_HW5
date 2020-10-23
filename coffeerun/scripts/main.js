71512465ef97d211e61147ced91be16c7cdfc3d0
aglawson <36459896+aglawson@users.noreply.github.com>
2020-10-23 14:45:18 -0700

    ,

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,34 +1,35 @@
-(function(window) {
-  'use strict';
-  var FORM_SELECTOR = '[data-coffee-order="form"]';
-  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
-  var SERVER_URL = 'http://localhost:2403/coffeeorders';
+(function (window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+    var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';     // CHOOSE ONLY ONE...
+    var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    // var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
 
-  var App = window.App;
-  var Truck = App.Truck;
-  var DataStore = App.DataStore;
-  var RemoteDataStore = App.RemoteDataStore;
-  var FormHandler = App.FormHandler;
-  var Validation = App.Validation;
-  var CheckList = App.CheckList;
+    var App = window.App;
+    var Truck = App.Truck;
+    var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
+    var FormHandler = App.FormHandler;
+    var Validation = App.Validation;
+    var CheckList = App.CheckList;
 
-  var remoteDS = new RemoteDataStore(SERVER_URL);
-  var myTruck = new Truck('ncc-1701', remoteDS);
-  window.myTruck = myTruck;
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    // var truck = new Truck('ncc-1701', new DataStore());
+    var truck = new Truck('ncc-1701', remoteDS);
+    window.truck = truck;
+    var checkList = new CheckList(CHECKLIST_SELECTOR);
+    checkList.addClickHandler(truck.deliverOrder.bind(truck));
+    var formHandler = new FormHandler(FORM_SELECTOR);
 
-  var checkList = new CheckList(CHECKLIST_SELECTOR);
-  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+    formHandler.addSubmitHandler(function(data) {
+        return truck.createOrder.call(truck, data)
+            .then(() => {
+                checkList.addRow.call(checkList, data);
+            }); 
+    });
+    console.log(formHandler);
 
-  var formHandler = new FormHandler(FORM_SELECTOR);
+    formHandler.addInputHandler(Validation.isCompanyEmail);
+    truck.printOrders(checkList.addRow.bind(checkList));
 
-  formHandler.addSubmitHandler(function(data) {
-    return myTruck.createOrder.call(myTruck, data)
-      .then(function() {
-        checkList.addRow.call(checkList, data);
-      });
-  });
-
-  formHandler.addInputHandler(Validation.isCompanyEmail);
-
-  myTruck.printOrders(checkList.addRow.bind(checkList));
 })(window);
--- coffeerun/scripts/truck.js
+++ coffeerun/scripts/truck.js
@@ -1,33 +1,39 @@
-(function(window) {
-  'use strict';
-  var App = window.App || {};
+(function (window) {
+    'use strict';
 
-  function Truck(truckId, db) {
-    this.truckId = truckId;
-    this.db = db;
-  }
-  Truck.prototype.createOrder = function(order) {
-    console.log('Adding order for ' + order.email);
-    return this.db.add(order.email, order);
-  };
-  Truck.prototype.deliverOrder = function(customerId) {
-    console.log('Delivering order for ' + customerId);
-    return this.db.remove(customerId);
-  };
-  Truck.prototype.printOrders = function(printFn) {
-    this.db.getAll()
-      .then(function(allData) {
-        var customerIdArray = Object.keys(allData);
-        console.log('Truck #' + this.truckId + ' has pending orders:');
-        customerIdArray.forEach(function(id) {
-          console.log(allData[id]);
-          if (printFn) {
-            printFn(allData[id]);
-          }
-        }.bind(this));
-      }.bind(this));
-  };
+    var App = window.App || {};
 
-  App.Truck = Truck;
-  window.App = App;
+    class Truck {
+        constructor(truckId, db) {
+            console.log('running the Truck function');
+            this.truckId = truckId;
+            this.db = db;
+        }
+        createOrder(order) {
+            console.log('Adding order for ' + order.emailAddress);
+            return this.db.add(order.emailAddress, order);
+        }
+        deliverOrder(customerId) { 
+            console.log('Delivering order for ' + customerId);
+            return this.db.remove(customerId);
+        }
+         printOrders(printfn) {
+            return this.db.getAll()
+            .then(function (orders) {
+                var customerIdArray = Object.keys(orders);
+
+                console.log(`Truck #${this.truckId} has pending orders:`);
+                customerIdArray.forEach(function(id) {
+                    console.log(orders[id]);
+                    if (printfn) { 
+                        printfn(orders[id]);
+                    }
+                }.bind(this));
+            }.bind(this));
+        };
+    }
+
+    App.Truck = Truck;
+    window.App = App;
+    
 })(window);
